2244873159975488174f9a852019540c5147e2ab
Nick Alteen <ncalteen@github.com>
2024-10-17 10:08:51 -0400

    Rebuild dist

--- dist/application.js
+++ dist/application.js
@@ -708,7 +708,6 @@ class GameManager {
                         tile.updatePosition(positions.next);
                         // Update the score
                         GameManager.state.score += merged.value;
-                        // GameManager.state.grid = Grid
                         // The mighty 2048 tile
                         if (merged.value === 2048)
                             GameManager.state.won = true;
